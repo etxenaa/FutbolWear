9bd1b2041b7590079581e2219ca73d181898d4f5
etxenaa <a_etxenagusia@fpzornotza.com>
2024-12-02 10:41:35 +0100

    a

---
+++ secondDates/src/main/java/com/secondDates/app/SecondDatesApplication.java
@@ -0,0 +1,13 @@
+package com.secondDates.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondDatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondDatesApplication.class, args);
+	}
+
+}
